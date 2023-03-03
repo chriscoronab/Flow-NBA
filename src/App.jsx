b43ee42b07aa595214e15341254b6cabb7f0d469
Christian Corona <chriscoronab@gmail.com>
2023-03-03 19:01:29 -0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
-import ItemListContainer from "./components/ItemListContainer";
+import Navbar from "./components/Navbar/index.jsx";
+import ItemListContainer from "./components/ItemListContainer/index.jsx";
 
 function App() {
   return (
