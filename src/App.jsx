2aef860fda10646be37c0cb967d84388db5d5f04
Christian Corona <chriscoronab@gmail.com>
2023-03-20 15:19:05 -0300

    mas correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { Navigate, Routes, Route } from "react-router-dom";
-import axios from "axios";
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
 import ProductList from "./components/ProductList";
 import ProductItem from "./components/ProductItem";
+import products from "../productos.json";
 
 function App() {
   const [productos, setProductos] = useState([]);
-  const getProductos = async () => {
-    try{
-      const result = await axios.get(`../productos.json`);
-      setProductos(result.data);
-    }catch(error){
-      console.log("ERROR:", error);
-    }
+  const getProductos = () => {
+  setProductos(products);
   };
   useEffect(() => {
-    getProductos();
+  getProductos();
   }, []);
   return (
     <div className="App">
